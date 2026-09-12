6c0e477ff49914e4e23e0053c1e4de51d63f50dc
yourAbhinav <abhinavkumark70@gmail.com>
2026-09-12 10:53:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='one' >
+
+   
+     <h1>Hello World !!!</h1>
+     <h2>this is my first react project !</h2>
+      </div>
+    </>
+    
+  )
+}
+export default App
+
